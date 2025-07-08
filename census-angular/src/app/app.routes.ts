ab56a16b6a238949da0a0533152cccea54d8387e
escarlet <escarlet@doctegrity.com>
2025-07-08 11:43:37 -0400

    Adding components and redirections

--- census-angular/src/app/app.routes.ts
+++ census-angular/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GroupComponent } from './group/group.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'groups', component: GroupComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'accounts', component: AccountsComponent },
+    { path: 'notifications', component: NotificationsComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
--- census-angular/src/app/header-nav/header-nav.component.ts
+++ census-angular/src/app/header-nav/header-nav.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProfileComponent } from '../profile/profile.component';
+import { AccountsComponent } from '../accounts/accounts.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './header-nav.component.html',
   styleUrl: './header-nav.component.css'
 })
