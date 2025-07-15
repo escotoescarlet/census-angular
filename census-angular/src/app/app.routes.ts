a2a5b64cb8b29271a890cf5cc9a5b298404714f9
kronos <mguerral1993@gmail.com>
2025-07-15 12:23:44 -0500

    feat: Company route added

--- census-angular/src/app/app.routes.ts
+++ census-angular/src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { NotificationsComponent } from './notifications/notifications.component'
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
+import {CompanyComponent} from "./companies/company.component";
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'main', component: MainComponent, children: [
         { path: 'dashboard', component: DashboardComponent },
         { path: 'groups', component: GroupComponent },
+        { path: 'companies', component: CompanyComponent },
         { path: 'profile', component: ProfileComponent },
         { path: 'accounts', component: AccountsComponent },
         { path: 'notifications', component: NotificationsComponent }
